0d6111bed64cc4fc69e298fc82b4771809264d6a
Brian Me <meduccuong@gmail.com>
2016-10-01 23:33:43 +0700

    Config Identity Server

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # oauth-secure-asp-dot-net-api
 Using OAuth to Secure Your ASP.NET API
+
+## OAuth
+https://www.scottbrady91.com/Identity-Server/Getting-Started-with-IdentityServer-4
+1. Install Identity Server
+```
+nuget: IdentityServer4
+```
+2. Update ConfigureServices 
+```
+services.AddIdentityServer()
+    .AddInMemoryStores()
+    .AddInMemoryClients(new List<Client>())
+    .AddInMemoryScopes(new List<Scope>())
+    .AddInMemoryUsers(new List<InMemoryUser>())
+    .SetTemporarySigningCredential();
+```
+3. Update Configure 
+```
+app.UseIdentityServer();
+```
+4. Check out the OpenID Connect Discovery Document 
+```
+    /.well-known/openid-configuration
+```
+5. OAuth Functionality
+```
+    POST /connect/token
+    Headers:
+    Content-Type: application/x-www-form-urlencoded
+    Body:
+    grant_type=client_credentials&scope=customAPI&client_id=oauthClient&client_secret=superSecretPassword
+```
+
--- SocialNetwork/SocialNetwork.OAuth/Startup.cs
+++ SocialNetwork/SocialNetwork.OAuth/Startup.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityServer4.Models;
+using IdentityServer4.Services.InMemory;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using SocialNetwork.OAuth.Models;
 
 namespace SocialNetwork.OAuth
 {
@@ -16,6 +19,12 @@ namespace SocialNetwork.OAuth
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentityServer()
+                .AddInMemoryStores()
+                .AddInMemoryClients(Clients.Get())
+                .AddInMemoryScopes(Scopes.Get())
+                .AddInMemoryUsers(Users.Get())
+                .SetTemporarySigningCredential();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,6 +36,8 @@ namespace SocialNetwork.OAuth
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseIdentityServer();
 
             app.Run(async (context) =>
             {
